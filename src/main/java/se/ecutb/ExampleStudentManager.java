e61182f0a2b883d6a6a5b280967759b1b1239fa0
crazvik <crazvik@gmail.com>
2021-03-15 10:40:36 +0100

    Updates

--- src/main/java/se/ecutb/ExampleStudentManager.java
+++ src/main/java/se/ecutb/ExampleStudentManager.java
@@ -4,6 +4,7 @@ import se.ecutb.data.CourseDaoList;
 import se.ecutb.data.StudentDaoList;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -12,8 +13,7 @@ public class ExampleStudentManager {
         Scanner scanner = new Scanner(System.in);
         StudentDaoList students = new StudentDaoList(new ArrayList<>());
         CourseDaoList courses = new CourseDaoList(new ArrayList<>());
-        String type, action, email, name;
-        int id;
+        String type, action;
         LocalDate localDate;
         StudentManager manager = new StudentManager(students, courses);
 
@@ -37,16 +37,13 @@ public class ExampleStudentManager {
                         switch (action.toLowerCase()) {
                             case "email":
                                 System.out.println("Enter email to search for");
-                                email = scanner.nextLine();
-                                manager.read(type, action, email, "", 0, LocalDate.MIN);
+                                manager.read(type, action, scanner.nextLine(), "", 0, LocalDate.MIN);
                                 break;
                             case "name":
-                                name = scanner.nextLine();
-                                manager.read(type, action, "", name, 0, LocalDate.MIN);
+                                manager.read(type, action, "", scanner.nextLine(), 0, LocalDate.MIN);
                                 break;
                             case "id":
-                                id = Integer.parseInt(scanner.nextLine());
-                                manager.read(type, action, "", "", id, LocalDate.MIN);
+                                manager.read(type, action, "", "", Integer.parseInt(scanner.nextLine()), LocalDate.MIN);
                                 break;
                             case "date":
                                 localDate = LocalDate.parse(scanner.nextLine());
